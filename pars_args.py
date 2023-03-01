2b47bd272b0cdddfc22711314ef4a5aa4f00a29f
Thring Zhou <179011938@qq.com>
2023-03-01 21:45:45 +0800

    更新 pars_args.py

--- pars_args.py
+++ pars_args.py
@@ -29,7 +29,7 @@ parser.add_argument('--lstm_layers',    type=int,   default=4)
 parser.add_argument('--n_epochs',       type=int,   default=300)
 parser.add_argument('--batch_size',     type=int,   default=256)
 parser.add_argument('--lr',             type=float, default=5e-4,
-                    help='5e-4 for Beijing, 1e-3 for Newyork')  # 2e-3
+                    help='5e-4 for Beijing, 1e-3 for Newyork')
 parser.add_argument('--save_epoch_int', type=int,   default=1)
 parser.add_argument('--save_folder',                default='saved_models')
 
